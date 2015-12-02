da4f92c7f94f070bcba4299c8569f5f214eb74d1
Shenglai Li <lyellooo@gmail.com>
2015-12-02 12:46:52 -0600

    Update muse_call.py

--- muse_call.py
+++ muse_call.py
@@ -1,14 +1,49 @@
 import os
 import sys
-#import bam_util
+import string
 import df_util
 import pipe_util
 import time_util
 
-def index_wxs()
+def fai_chunk(fai_path, blocksize):
+  seq_map = {}
+  with open(fai_path) as handle:
+    for line in handle:
+      tmp = line.split("\t")
+      seq_map[tmp[0]] = long(tmp[1])
+    for seq in seq_map:
+        l = seq_map[seq]
+        for i in xrange(1, l, blocksize):
+            yield (seq, i, min(i+blocksize-1, l))
+            
+def muse_call_cmd_iter(muse, ref, fai_path, blocksize, tumor_bam, normal_bam, output_base):
+  template = string.Template("${MUSE} call -f ${REF} -r ${REGION} ${TUMOR_BAM} ${NORMAL_BAM} -O ${OUTPUT_BASE}.${BLOCK_NUM}")
+  for i, block in enumerate(fai_chunk(fai_path, blocksize)):
+    cmd = template.substitute(
+                              dict(
+                                   REF = ref,
+                                   REGION = '%s:%s-%s' % (block[0], block[1], block[2]),
+                                   BLOCK_NUM = i
+                                   ),
+                                   MUSE = muse,
+                                   TUMOR_BAM = tumor_bam,
+                                   NORMAL_BAM = normal_bam,
+                                   OUTPUT_BASE = output_base
+                              )
+    yield cmd, "%s.%s.MuSE.txt" % (output_base, i)
 
-def index_wgs()
-
-def call_wxs()
-
-def call_wgs()
+def call(uuid, thread_count, analysis_ready_tumor_bam_path, analysis_ready_normal_bam_path, reference_fasta_name, fai_path, blocksize, engine, logger):
+  call_dir = os.path.dirname(analysis_ready_tumor_bam_path)
+  tumor_bam_name = os.path.basename(analysis_ready_tumor_bam_path)
+  tb_base, tb_ext = os.path.splitext(tumor_bam_name)
+  logger.info('MuSE_call_dir=%s' % call_dir)
+  step_dir = call_dir
+  muse_call_output = tb_base + '.MuSE.txt'
+  muse_call_output_path = os.path.join(call_dir, muse_call_output)
+  logger.info('muse_call_output_path=%s' % muse_call_output_path)
+  if pipe_util.already_step(step_dir, tumor_bam_name + '_MuSE_call', logger):
+    logger.info('already completed step `MuSE call` of: %s' % analysis_ready_tumor_bam_path)
+  else:
+    logger.info('running step `MuSE call` of: %s' % analysis_ready_tumor_bam_path)
+    
+  
