54c9dc52aa03fba94a055898985f415f0c151fab
Shenglai Li <lyellooo@gmail.com>
2015-12-11 17:06:18 -0600

    Update pipe_util.py

--- pipe_util.py
+++ pipe_util.py
@@ -222,9 +222,8 @@ def remove_dir(adir, engine, logger):
 def do_pool_commands(cmd, logger, lock=Lock()):
     #logger.info('running cmd: %s' % cmd)
     output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output_stdout = output.communicate()[0]
-    with lock:
-        logger.info('contents of output=%s' % output_stdout.decode().format())
+    output_stdout = output.communicate()
+    logger.info('contents of output=%s' % str(output_stdout).decode().format())
     #logger.info('completed cmd: %s' % str(cmd))
     return output.wait()
     
