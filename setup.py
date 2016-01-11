aca17a306c3639d76efc51506dc33f147bc7e358
Shenglai Li <sli6@uchicago.edu>
2016-01-11 14:00:26 -0600

    add setup files needed for pip

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name = 'muse-tool',
+      author = 'Shenglai Li',
+      author_email = 'sli6@uchicago.edu',
+      version = 0.1,
+      description = 'Baylor MuSE tools',
+      url = 'https://github.com/NCI-GDC/cocleaning-tool',
+      license = 'Apache 2.0',
+      packages = find_packages(),
+      install_requires = [
+          'pandas',
+          'sqlalchemy',
+          'cdis_pipe_utils'
+      ],
+      classifiers = [
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ],
+)
