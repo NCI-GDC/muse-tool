172ed3c1f710c338ff676dfff2cb9a7809d1ae90
Shenglai Li <sli6@uchicago.edu>
2016-01-13 09:58:07 -0600

    fix(main.py): remove dbsnp requirement

--- muse-tool/main.py
+++ muse-tool/main.py
@@ -45,7 +45,6 @@ def main():
                         help = 'Reference fasta fai path.',
     )
     parser.add_argument('-snp','--dbsnp_known_snp_sites',
-                        required=True,
                         help='Reference SNP path, that should be bgzip compressed, tabix indexed',
     )
     parser.add_argument('-tb', '--tumor_bam_path',
