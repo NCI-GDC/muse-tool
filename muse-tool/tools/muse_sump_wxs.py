26ab6a359c14c4473db470239fd72dc3db98faaa
Shenglai Li <sli6@uchicago.edu>
2016-01-13 10:14:29 -0600

    fix(muse_sump.py): change variable name

--- muse-tool/tools/muse_sump_wxs.py
+++ muse-tool/tools/muse_sump_wxs.py
@@ -12,7 +12,7 @@ def sump_wxs(uuid, muse_call_output_path, dbsnp_known_snp_sites, engine, logger)
     muse_sump_output = input_base + '.vcf'
     muse_sump_output_path = os.path.join(step_dir, muse_sump_output)
     logger.info('muse_sump_output_path=%s' % muse_sump_output_path)
-    if pipe_util.already_step(step_dir, sample_base + '_MuSE_sump', logger):
+    if pipe_util.already_step(step_dir, uuid + '_MuSE_sump', logger):
         logger.info('already completed step `MuSE sump` of: %s' % input_name)
     else:
         logger.info('running step `MuSE sump` of the tumor bam: %s' % input_name)
@@ -26,6 +26,6 @@ def sump_wxs(uuid, muse_call_output_path, dbsnp_known_snp_sites, engine, logger)
         unique_key_dict = {'uuid': uuid, 'muse_call_output': muse_call_output_path, 'muse_sump_output': muse_sump_output_path}
         table_name = 'time_mem_MuSE_sump_wxs'
         df_util.save_df_to_sqlalchemy(df, unique_key_dict, table_name, engine, logger)
-        pipe_util.create_already_step(step_dir, sample_base + '_MuSE_sump', logger)
+        pipe_util.create_already_step(step_dir, uuid + '_MuSE_sump', logger)
         logger.info('completed running `MuSE sump` of the tumor bam: %s' % input_name)
     return muse_sump_output_path
