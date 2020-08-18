7fa8ecdbd2ad1251d51a661bc9d29be6a2d722d4
Charles Czysz <czysz@uchicago.edu>
2020-08-18 09:55:10 -0500

    Test suite

---
+++ tests/test_multi_muse.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import pathlib
+import subprocess
+import unittest
+
+from muse_tool import multi_muse as MOD
+
+
+class ThisTestCase(unittest.TestCase):
+    pass
+
+
+# __END__
