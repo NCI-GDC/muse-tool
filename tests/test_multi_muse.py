7bc84761dc731655c0c2b98d3f40339440751b7d
Charles Czysz <czysz@uchicago.edu>
2020-08-21 09:46:32 -0500

    Add test

--- tests/test_multi_muse.py
+++ tests/test_multi_muse.py
@@ -3,12 +3,48 @@
 import pathlib
 import subprocess
 import unittest
+from types import SimpleNamespace
+from unittest import mock
 
 from muse_tool import multi_muse as MOD
 
 
 class ThisTestCase(unittest.TestCase):
-    pass
+    def setUp(self):
+        super().setUp()
+        self.mocks = SimpleNamespace(
+            subprocess=mock.MagicMock(spec_set=subprocess),
+            threading=mock.MagicMock(spec_set=MOD.threading),
+        )
+
+    def setup_popen(self, stdout=None, stderr=None):
+        stdout = stdout or b''
+        stderr = stderr or b''
+        popen_instance = mock.MagicMock(subprocess.Popen)
+        self.mocks.subprocess.Popen.return_value = popen_instance
+        popen_instance.communicate.return_value = (stdout, stderr)
+        return popen_instance
+
+
+class Test_subprocess_commands_pipe(ThisTestCase):
+    def setUp(self):
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_Popen_called_as_expected(self):
+        mock_popen = self.setup_popen()
+        cmd = MOD.CMD_STR
+        pre_exec_fn = lambda: True
+        MOD.subprocess_commands_pipe(cmd, pre_exec_fn=pre_exec_fn, di=self.mocks)
+        self.mocks.subprocess.Popen.assert_called_once_with(
+            MOD.shlex.split(cmd),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            preexec_fn=pre_exec_fn(),
+        )
+        mock_popen.communicate.assert_called_once_with()
 
 
 # __END__
