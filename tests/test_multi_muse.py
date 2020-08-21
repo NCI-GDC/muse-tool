82e13e7a82ff82ccb7076445037100881177fec3
Charles Czysz <czysz@uchicago.edu>
2020-08-21 14:44:28 -0500

    Update unit test

--- tests/test_multi_muse.py
+++ tests/test_multi_muse.py
@@ -12,10 +12,7 @@ from muse_tool import multi_muse as MOD
 class ThisTestCase(unittest.TestCase):
     def setUp(self):
         super().setUp()
-        self.mocks = SimpleNamespace(
-            subprocess=mock.MagicMock(spec_set=subprocess),
-            threading=mock.MagicMock(spec_set=MOD.threading),
-        )
+        self.mocks = SimpleNamespace(subprocess=mock.MagicMock(spec_set=subprocess),)
 
     def setup_popen(self, stdout=None, stderr=None):
         stdout = stdout or b''
@@ -36,15 +33,11 @@ class Test_subprocess_commands_pipe(ThisTestCase):
     def test_Popen_called_as_expected(self):
         mock_popen = self.setup_popen()
         cmd = MOD.CMD_STR
-        pre_exec_fn = lambda: True
-        MOD.subprocess_commands_pipe(cmd, pre_exec_fn=pre_exec_fn, di=self.mocks)
+        MOD.subprocess_commands_pipe(cmd, di=self.mocks)
         self.mocks.subprocess.Popen.assert_called_once_with(
-            MOD.shlex.split(cmd),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            preexec_fn=pre_exec_fn(),
+            MOD.shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE,
         )
-        mock_popen.communicate.assert_called_once_with()
+        mock_popen.communicate.assert_called_once_with(timeout=3600)
 
 
 # __END__
