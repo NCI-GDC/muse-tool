0ba5c776c1602c349f66866b77294887a31926a3
Shenglai <lyellooo@gmail.com>
2021-03-11 20:51:47 +0000

    BINF-481: fix * add version to argparse * raise any exceptions from threaded calls * property mock for retcode * always push hash tag image * log failing commands * skip test

--- muse_tool/__init__.py
+++ muse_tool/__init__.py
@@ -1,3 +1,6 @@
-from muse_tool._version import __short_version__
+try:
+    from muse_tool._version import __long_version__
 
-__version__ = __short_version__
+    __version__ = __long_version__
+except ImportError:
+    __version__ = "0.0.0"
